1983d893c75ae6bd62faa27a26dc4ddc32096651
JetLearnAdhayan <adhayanraj9@gmail.com>
2025-06-15 12:28:11 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -2,15 +2,11 @@
 import numpy as np
 import pandas as pd
 
-data = pd.read_csv("titanic.csv")
+data = pd.read_csv('titanic.csv')  
+data['Sex']=data['Sex'].map({'male':0,'female':1})
 print(data.head())
-
-data = data["Sex"]=data["Sex"].map({"male": 0,"female":1})
-print(data.head())
-#print(data["Age"])
-
 x = data[['Age','Sex','Pclass']]
-y = data["Survived"]
+y = data['Survived']
 
 from sklearn.model_selection import train_test_split
 
@@ -28,3 +24,19 @@ y_predict = linearreg.predict(x_test)
 rmse_lr = (np.sqrt(mean_squared_error(y_test,y_predict)))
 
 print("The rmse of MultiVariable Regression is:", rmse_lr)
+
+#polynominal regression
+
+from sklearn.preprocessing import PolynomialFeatures
+poly_features = PolynomialFeatures(degree = 2)
+
+x_train_poly = poly_features.fit_transform(x_train)
+
+poly_model = LinearRegression()
+poly_model.fit(x_train_poly,y_train)
+
+x_test_poly = poly_features.fit_transform(x_test)
+y_test_predict_poly = poly_model.predict(x_test_poly)
+
+rmse_poly_model = (np.sqrt(mean_squared_error(y_test, y_test_predict_poly)))
+print("the rmse in case of polynominial regression is", rmse_poly_model)
